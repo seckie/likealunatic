d4bce3b008fae0051e815f9bc3e1c8cb94f2e556
Naoki Sekiguchi <naokis@ranagram.com>
2017-04-14 13:54:20 +0900

    Update pagination style

--- public/wp-content/themes/likealunatic30/single.php
+++ public/wp-content/themes/likealunatic30/single.php
@@ -10,30 +10,25 @@
 get_header(); ?>
 
 <div class="container area_bd">
-<div class="row">
-    <div class="span12" id="primary">
+  <div class="row">
+    <div class="col-md-8 col-xs-12" id="primary">
       <div id="content" role="main">
 
         <?php while ( have_posts() ) : the_post(); ?>
-
           <?php get_template_part( 'content', 'single' ); ?>
-
-          <nav class="pagination" id="nav-single">
-            <ul>
-            <li class="prev nav-previous"><?php previous_post_link( '%link', '&larr; %title'  ); ?></li>
-            <li class="next nav-next"><?php next_post_link( '%link', ' %title &rarr;' ); ?></li>
+          <nav class="pagination-wrapper" id="nav-single">
+            <ul class="pagination pagination-lg">
+              <li class="prev nav-previous"><?php previous_post_link( '%link', '&larr; %title'  ); ?></li>
+              <li class="next nav-next"><?php next_post_link( '%link', ' %title &rarr;' ); ?></li>
             </ul>
-          </nav><!-- #nav-single -->
+          <!-- #nav-single --></nav>
 
           <?php comments_template( '', true ); ?>
-
         <?php endwhile; // end of the loop. ?>
 
-      </div><!-- #content -->
-    </div><!-- #primary -->
-
+      <!--/#content--></div>
+    <!--/#primary--></div>
 <?php get_sidebar(); ?>
-
-<!--/.row--></div>
+  <!--/.row--></div>
 <!--/.container area_bd--></div>
 <?php get_footer(); ?>
