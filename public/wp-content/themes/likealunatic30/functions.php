baf1dc388859b9ba884ff194b37fa28b9309357e
Naoki Sekiguchi <web@likealunatic.jp>
2017-11-23 17:30:55 +0900

    Add meta tags (title, description and og tags)

--- public/wp-content/themes/likealunatic30/functions.php
+++ public/wp-content/themes/likealunatic30/functions.php
@@ -40,3 +40,23 @@ header("X-XSS-Protection: 1; mode=block");
 header("X-Content-Type-Options: nosniff");
 header("Referrer-Policy: no-referrer-when-downgrade");
 
+
+function the_excerpt_max_charlength($charlength) {
+  $excerpt = get_the_excerpt();
+  $charlength++;
+
+  if ( mb_strlen( $excerpt ) > $charlength ) {
+    $subex = mb_substr( $excerpt, 0, $charlength - 5 );
+    $exwords = explode( ' ', $subex );
+    $excut = - ( mb_strlen( $exwords[ count( $exwords ) - 1 ] ) );
+    if ( $excut < 0 ) {
+      echo mb_substr( $subex, 0, $excut );
+    } else {
+      echo $subex;
+    }
+    echo '[...]';
+  } else {
+    echo $excerpt;
+  }
+}
+
