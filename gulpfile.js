501398e114c0cacbeda950717eb089b84af6696a
Naoki Sekiguchi <web@likealunatic.jp>
2019-06-30 17:28:35 +0900

    Upgrade gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,19 @@
-'use strict';
-// gulp tasks
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var webpack = require('webpack');
-var gulpWebpack = require('gulp-webpack');
-var eslint = require('gulp-eslint');
-var notify = require('gulp-notify');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var plumber = require('gulp-plumber');
+const {src, dest, parallel, watch, series} = require('gulp');
+const gutil = require('gulp-util');
+const webpack = require('webpack');
+const gulpWebpack = require('gulp-webpack');
+const eslint = require('gulp-eslint');
+const notify = require('gulp-notify');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const plumber = require('gulp-plumber');
 
 // constants
-var PUBLIC_PATH = 'public/';
-var WP_THEME_PATH = 'public/wp-content/themes/likealunatic30/';
-var SRC_PATH = './src/';
-var GULPFILE_PATH = './gulpfile.js';
-var PATHS = {
+const PUBLIC_PATH = 'public/';
+const WP_THEME_PATH = 'public/wp-content/themes/likealunatic30/';
+const SRC_PATH = './src/';
+const GULPFILE_PATH = './gulpfile.js';
+const PATHS = {
   jsSrc: [SRC_PATH + 'js/**/*.js'],
   jsSrcMain: SRC_PATH + 'js/main.js',
   js: [WP_THEME_PATH + '**/*.js'],
@@ -36,24 +34,24 @@ function errorHandler (err, stats) {
 }
 
 // build CSS
-gulp.task('sass', function () {
-  return gulp.src(PATHS.sass)
+function taskSass() {
+  return src(PATHS.sass)
     .pipe(plumber({ errorHandler: errorHandler }))
     .pipe(sass({ outputStyle: 'expanded' }))
-    .pipe(gulp.dest(PATHS.cssDir))
+    .pipe(dest(PATHS.cssDir))
     .pipe(browserSync.stream());
-});
+}
 
 // build JavaScript
-gulp.task('eslint', function () {
-  return gulp.src(PATHS.jsSrc.concat([GULPFILE_PATH]))
+function taskEslint() {
+  return src(PATHS.jsSrc.concat([GULPFILE_PATH]))
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
-});
+}
 
 // webpack
-var WEBPACK_OPT = {
+const WEBPACK_OPT = {
   entry: { script: PATHS.jsSrcMain },
   output: {filename: '[name].js'},
   module: {
@@ -66,14 +64,15 @@ var WEBPACK_OPT = {
   },
   externals: { }
 };
-gulp.task('build', function () {
-  return gulp.src(PATHS.jsSrcMain)
-    .pipe(gulpWebpack(WEBPACK_OPT, null, errorHandler))
-    .pipe(gulp.dest(PATHS.jsDir))
-    .pipe(browserSync.stream());
-});
 
-var WEBPACK_COMPRESS_OPT = {
+function taskBuild() {
+  return src(PATHS.jsSrcMain)
+    .pipe(gulpWebpack(WEBPACK_OPT, null, errorHandler))
+    .pipe(dest(PATHS.jsDir))
+    .pipe(browserSync.stream());
+}
+
+const WEBPACK_COMPRESS_OPT = {
   output: { filename: '[name].min.js' },
   plugins: [
     new webpack.DefinePlugin({
@@ -81,6 +80,8 @@ var WEBPACK_COMPRESS_OPT = {
         NODE_ENV: JSON.stringify('production')
       }
     }),
+    /*
+    // Error: webpack.optimize.UglifyJsPlugin has been removed, please use config.optimization.minimize instead.
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false },
       comments: function (astNode, comment) {
@@ -89,25 +90,26 @@ var WEBPACK_COMPRESS_OPT = {
       },
       mangle: false
     })
+    */
   ]
 };
-gulp.task('compress', function () {
-  var opt = Object.assign({}, WEBPACK_OPT, WEBPACK_COMPRESS_OPT);
-  return gulp.src(PATHS.jsSrcMain)
+function taskCompress() {
+  const opt = Object.assign({}, WEBPACK_OPT, WEBPACK_COMPRESS_OPT);
+  return src(PATHS.jsSrcMain)
     .pipe(gulpWebpack(opt, null, errorHandler))
-    .pipe(gulp.dest(PATHS.jsDir))
+    .pipe(dest(PATHS.jsDir))
     .pipe(browserSync.stream());
-});
+}
 
 // server
-gulp.task('browser-sync', function () {
+function taskBrowserSync() {
   browserSync.init({
     open: false,
     server: {
       baseDir: PUBLIC_PATH,
       middleware: [
         function (req, res, next) {
-          var msg = req.method;
+          let msg = req.method;
           msg += ' ';
           msg += req.url;
           msg += '  ';
@@ -120,16 +122,20 @@ gulp.task('browser-sync', function () {
       ]
     }
   });
-});
+}
 
 // watch
-gulp.task('watch', function () {
+function taskWatch() {
   gutil.log('start watching');
-  gulp.watch(PATHS.sass, ['sass']);
-  gulp.watch(PATHS.jsSrc, ['eslint', 'build']);
-  gulp.watch([GULPFILE_PATH], ['eslint']);
-});
+  watch(PATHS.sass, taskSass);
+  watch(PATHS.jsSrc, series(taskEslint, taskBuild));
+  watch([GULPFILE_PATH], taskEslint);
+}
 
-// commands
-gulp.task('default', ['browser-sync', 'sass', 'eslint', 'watch']);
-
+exports.sass = taskSass;
+exports.eslint = taskEslint;
+exports.build = taskBuild;
+exports.compress = taskCompress;
+exports.browserSync = taskBrowserSync;
+exports.watch = taskWatch;
+exports.default = parallel(taskBrowserSync, taskSass, taskEslint, taskWatch);
